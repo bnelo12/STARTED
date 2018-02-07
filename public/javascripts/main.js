f29bf718d77d1f3e9055682f943948f2a43f5240
https://bnelo12@github.com <s1623919@sms.ed.ac.uk>
2018-02-07 14:35:47 +0000

    Added partner brands

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -31,6 +31,43 @@ $('document').ready(function() {
             $('#menu-bar').css({position: 'absolute', top: ($(window).height() - 70), width: ($(window).width())});
         }
     };
+    $('.your-class').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
 });
 
 var playIntroAnimation = function() {
