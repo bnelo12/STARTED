68dc85d88b2f432b18b88b1f4e33470a6d868da3
https://bnelo12@github.com <s1623919@sms.ed.ac.uk>
2018-02-10 22:53:07 +0000

    Added Google site verification

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -24,20 +24,59 @@ $('document').ready(function() {
             scrollTop: ($( $.attr(this, 'href') ).offset().top - 70)
         }, 500);
     });
+
     window.onresize = function(event) {
         if($(window).scrollTop() > $(window).height() - 70) {
-            $('#menu-bar').css({position: 'fixed', top: "0px", backgroundColor: "rgba(52, 58, 64, 0.8) !important"});
+            $('#menu-bar').css({position: 'fixed', top: "0px", backgroundColor: "rgba(52, 58, 64, 0.8) !important", width: ($(window).width())});
         } else {
             $('#menu-bar').css({position: 'absolute', top: ($(window).height() - 70), width: ($(window).width())});
         }
     };
+
     $('.your-class').slick({
         dots: true,
         infinite: true,
         speed: 300,
         slidesToShow: 4,
         slidesToScroll: 1,
-        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+    $('.schedule-carousel').slick({
+        dots: true,
+        infinite: true,
+        slide: '.schedule-item',
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
         responsive: [
